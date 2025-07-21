3f93855f889940cbed4f5a8e24b0456307c0fd43
mvestrand <10858947+mvestrand@users.noreply.github.com>
2025-07-21 12:16:33 -0600

    Various changes

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,64 @@
 #include <flecs.h>
 
 
+// struct engine {
+//     const float SIMULATION_TICK_LENGTH = 0.016667f;
+//     struct PhysicsUpdate {};
+//     struct SimulationTick {};
+
+//     engine(flecs::world &world) {
+//         physics_tick = world.timer<SimulationTick>().interval(SIMULATION_TICK_LENGTH);
+//         world.component<PhysicsUpdate>()
+//             .add(flecs::Phase)
+//             .tick_source(physics_tick)
+//             .depends_on(flecs::PreUpdate);
+
+//         world.system("PrintTime")
+//             .kind(flecs::OnUpdate)
+//             .run([](flecs::iter& it) {
+//                 // printf("Time: %f\n", it.delta_time());
+//             });
+//     }
+//     flecs::entity physics_tick;
+// };
+using flecs::world;
+using flecs::entity;
+
 struct engine {
-    const float SIMULATION_TICK_LENGTH = 0.016667f;
-    struct UpdateSimulation {};
-    struct SimulationTick {};
+    
 
-    engine(flecs::world &world) {
-        physics_tick = world.timer<SimulationTick>().interval(SIMULATION_TICK_LENGTH);
-        world.component<UpdateSimulation>()
-            .add(flecs::Phase)
-            .depends_on(flecs::OnUpdate);
 
-        world.system("PrintTime")
-            .kind(flecs::OnUpdate)
-            .run([](flecs::iter& it) {
-                printf("Time: %f\n", it.delta_time());
-            });
+
+    engine(world &ecs) {
+
     }
-    flecs::entity physics_tick;
 };
+
+
+
 
 int main(int argc, char *argv[]) {
     flecs::world world(argc, argv);
+    world.import<flecs::stats>();
+    world.set<flecs::Rest>({});
+
     world.import<engine>();
-    return world.app()
-        .enable_rest()
-        .target_fps(60)
-        .delta_time(1.0f / 60.0f)
-        .run();
+
+
+    while (world.progress()) {
+
+    }
+
+    // Start Frame
+    // - Fixed time-step(s) (16 ms)
+    //      - Input
+    //      - Physics update
+    // - Frame time-step
+    //      - Input
+    //      - Gameplay update
+    //      - UI
+    // - Draw frame
+    // End Frame
+
+    return 0;
 }
